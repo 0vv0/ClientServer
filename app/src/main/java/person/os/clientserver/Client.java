8d991cca5a8fece6fcaf5d045d0753095d37f97e
0vv0 <softina@uk.net>
2018-11-02 12:34:37 +0200

    START

---
+++ app/src/main/java/person/os/clientserver/Client.java
@@ -0,0 +1,153 @@
+package person.os.clientserver;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client extends Thread {
+    private static final String TAG = Client.class.getName();
+
+    public InetAddress getAddress() {
+        return socket.getInetAddress();
+    }
+
+    private final Socket socket;
+    private final Handler handler;
+    private final Thread receivingThread;
+    private final Thread sendingThread;
+    private volatile String msg;
+
+    public Client(Socket client, Handler handler) throws IOException {
+        if (client == null || handler == null) {
+            throw new IllegalArgumentException("socket and handler should not be null");
+        }
+        this.socket = client;
+        this.handler = handler;
+        this.receivingThread = new Thread(new ReceivingThread(socket.getInputStream()));
+        this.sendingThread = new Thread(new SendingThread(socket.getOutputStream()));
+        start();
+    }
+
+    @Override
+    public void run() {
+        receivingThread.start();
+        sendingThread.start();
+        while (!isInterrupted()) {
+            yield();
+        }
+
+    }
+
+    public synchronized void send(String msg) {
+        this.msg = msg;
+    }
+
+    private synchronized String getMsg() {
+        return msg;
+    }
+
+    public synchronized void receive(String msg) {
+        log(msg);
+        Message message = handler.obtainMessage();
+        Bundle bundle = message.getData();
+        bundle.putString("key", msg);
+        message.setData(bundle);
+        handler.sendMessage(message);
+    }
+
+    private void log(String msg) {
+        Log.e(TAG, msg);
+    }
+
+    private static void log(Exception e) {
+        Log.e(TAG, e.getLocalizedMessage());
+    }
+
+    @Override
+    public void interrupt() {
+        Log.e(TAG, "interrupted");
+        super.interrupt();
+        receivingThread.interrupt();
+        sendingThread.interrupt();
+
+        try {
+            socket.getInputStream().close();
+        } catch (IOException e) {
+            log(e);
+        }
+        try {
+            socket.getOutputStream().close();
+        } catch (IOException e) {
+            log(e);
+        }
+        try {
+            socket.close();
+        } catch (IOException e) {
+            log(e);
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return socket.equals(obj);
+    }
+
+    private class ReceivingThread implements Runnable {
+        private final BufferedReader br;
+
+        private ReceivingThread(InputStream clientInputStream) {
+            br = new BufferedReader(new InputStreamReader(clientInputStream));
+        }
+
+
+        @Override
+        public void run() {
+            while (!isInterrupted()) {
+                try {
+                    if (br.ready()) {
+                        StringBuffer sb = new StringBuffer();
+                        while (br.ready()) {
+                            sb.append(br.readLine());
+                        }
+                        receive(sb.toString());
+                    } else {
+                        yield();
+                    }
+                } catch (IOException e) {
+                    log(e);
+                }
+            }
+        }
+    }
+
+    private class SendingThread implements Runnable {
+        private final BufferedWriter bw;
+
+        private SendingThread(OutputStream clientOutputStream) {
+            this.bw = new BufferedWriter(new OutputStreamWriter(clientOutputStream));
+        }
+
+        @Override
+        public void run() {
+            while (!isInterrupted()) {
+                String msg = getMsg();
+                if (msg != null) {
+                    try {
+                        bw.write(msg);
+                        bw.flush();
+                        send(null);
+                    } catch (IOException e) {
+                        log(e);
+                    }
+                } else {
+                    yield();
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/person/os/clientserver/Server.java
@@ -0,0 +1,86 @@
+package person.os.clientserver;
+
+import android.os.Handler;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Server extends Thread {
+    private static final String TAG = Server.class.getName();
+    private final Set<Client> clients = Collections.newSetFromMap(new ConcurrentHashMap<Client, Boolean>());
+    private volatile int port;
+    private final Handler handler;
+
+    public Server(int port, Handler handler) {
+        if (port <= 0 || port > 65535) {
+            throw new IllegalArgumentException("Use 0<port<=65535");
+        }
+        this.port = port;
+        this.handler = handler;
+    }
+
+    public void addClient(Socket client) throws IOException {
+        Client clientThread = new Client(client, handler);
+        clients.add(clientThread);
+    }
+
+    public void removeClient(Socket client) {
+        for (Client cl : clients) {
+            if(cl.getAddress().equals(client.getInetAddress())){
+                clients.remove(cl);
+                break;
+            }
+        }
+    }
+
+    private static void log(Exception e) {
+        Log.e(TAG, e.getLocalizedMessage());
+    }
+
+    @Override
+    public void run() {
+        try {
+            ServerSocket ss = new ServerSocket(port);
+            port = ss.getLocalPort();
+            while (!Thread.interrupted()) {
+                clientsCleanup();
+                Socket client = ss.accept();
+                addClient(client);
+            }
+        } catch (IOException e) {
+            Log.e(TAG, e.getLocalizedMessage());
+        }
+    }
+
+    private void clientsCleanup(){
+        Set<Client> clientsToRemove = new HashSet<>();
+        for (Client client : clients) {
+            if(!client.isAlive()){
+                clientsToRemove.add(client);
+            }
+        }
+        clients.removeAll(clientsToRemove);
+    }
+
+    public void tierDown(){
+        for (Client client : clients) {
+            client.interrupt();
+        }
+    }
+
+    @Override
+    public void interrupt() {
+        super.interrupt();
+        tierDown();
+    }
+
+    public synchronized int getPort() {
+        return port;
+    }
+}
