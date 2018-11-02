a933319404763aea1a64ecff7983b0b7bfcc2c45
0vv0 <softina@uk.net>
2018-11-02 16:16:34 +0200

    main activity

--- app/src/main/java/person/os/checkers/ui/MainActivity.java
+++ app/src/main/java/person/os/checkers/ui/MainActivity.java
@@ -14,12 +14,16 @@ import person.os.clientserver.Client;
 import person.os.clientserver.R;
 import person.os.clientserver.Server;
 
-public class MainActivity extends AppCompatActivity implements Handler.Callback{
+import java.io.IOException;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity implements Handler.Callback {
     Server server;
     Client client;
     TextView msg;
     TextView log;
     Button ss;
+    Button cl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,20 +37,63 @@ public class MainActivity extends AppCompatActivity implements Handler.Callback{
         ss.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (server == null){
-                    server = new Server(3550, new Handler(getMainLooper(), MainActivity.this));
-                    server.start();
+                serverInitialization();
+            }
+        });
+
+        cl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (true) {
                 }
             }
         });
+
+
+    }
+
+    private void serverInitialization() {
+        if (server == null || !server.isAlive()) {
+            server = new Server(3550, new Handler(getMainLooper(), MainActivity.this));
+            server.start();
+        }
+        ss.setText("Working on: " + server.getName() + ":" + server.getPort());
+    }
+
+    private void clientInitialization() {
+        if (client == null || !client.isAlive()) {
+
+            try {
+                client = new Client(
+                        new Socket(
+                                ((TextView) findViewById(R.id.host)).getText().toString(),
+                                Integer.valueOf(((TextView) findViewById(R.id.port)).getText().toString()))
+                        , new Handler(this.getMainLooper(), this));
+            } catch (IOException e) {
+                log.setText(e.getLocalizedMessage());
+            }
+
+        }
     }
 
     @Override
     public boolean handleMessage(Message msg) {
-        if(msg.getData().getString("key")!=null){
+        if (msg.getData().getString("key") != null) {
             log.setText(msg.getData().getString("key"));
         }
 
         return false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (server != null) {
+            server.interrupt();
+        }
+        if (client != null) {
+            client.interrupt();
+        }
     }
 }
